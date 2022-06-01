9dc961958ee2a7dd6278a602ba72a6745e0493a6
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-06-01 11:28:21 +0300

    commit tree 49c43308e21b0cf6ce293d5bb4dd226eba48af23

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+// const cats =["Milo", "Otis", "Garfield"];
+// function destructively(cats){
+
+
+// }
+// destructively();
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat =  (name) => {
+    return cats.push(name);
+}
+
+const destructivelyPrependCat =  (name) => {
+    return cats.unshift(name);
+}
+
+const destructivelyRemoveLastCat = () => {
+    return cats.pop();
+}
+
+const destructivelyRemoveFirstCat = () => {
+    return cats.shift();
+}
+
+const appendCat = (name) => {
+    const newCats = [...cats, name];
+    return newCats;
+}
+
+const prependCat = (name) => {
+    const newCats = [name, ...cats];
+    return newCats;
+}
+
+const removeLastCat = () => {
+    const newCats = cats.slice(0, -1)
+    return newCats
+}
+
+const removeFirstCat = () => {
+    const newCats = cats.slice(1)
+    return newCats
+}
